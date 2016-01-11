72f3851bacc510d648acc9bcfbd19c26fc155ffc
도플광어 <gbox3d@gmail.com>
2016-01-11 14:40:49 +0900

    iscroll

--- sample/4.scroll/amd/app/panel/testPage/control.js
+++ sample/4.scroll/amd/app/panel/testPage/control.js
@@ -7,6 +7,7 @@ define(
 
         //디랙티브와 연결
         var this_dom;
+        var Scroll_obj;
 
         return {
             setup : function(name) {
@@ -14,10 +15,22 @@ define(
                 this_dom = document.querySelector("#amd-popup-" + name);
                 this_dom.classList.add('hide');
 
+                Scroll_obj = new IScroll(this_dom.querySelector('#wrapper'),
+                    {
+                        mouseWheel : true
+                    }
+                );
+
+
+
             },
             show : function(option) {
                 //화면 재구성
                 this_dom.classList.remove('hide');
+
+                setTimeout(function() {
+                    Scroll_obj.refresh();
+                },300)
 
             },
             hide : function() {
